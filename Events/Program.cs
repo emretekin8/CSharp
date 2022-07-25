d613fca7553999a4943405589bab876be11081c3
emretekin8 <emretekin601@gmail.com>
2022-07-25 11:36:47 +0300

    Events added

---
+++ Events/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product screen = new Product(50);
+            screen.ProductName = "SCREEN";
+
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockControlEvent += GsmStockControlEvent;
+
+            for (int i = 0; i < 10; i++)    
+            {
+                screen.Sell(10);
+                gsm.Sell(5);
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void GsmStockControlEvent()
+        {
+            Console.WriteLine("GSM about to finish!!!");
+        }
+    }
+}
